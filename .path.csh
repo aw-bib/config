00620ea8701649589a63bf4dc1599b7639c7ed08
Alexander Wagner <a.wagner@stellarcom.org>
2014-05-30 10:28:31 +0200

    Default search paths for tcsh and sh like shells.

---
+++ .path.csh
@@ -0,0 +1,93 @@
+#!/bin/tcsh
+#
+# Set up the path source it from .login and .tcshrc
+if ( $HOSTTYPE == "alpha" ) then
+	if ($OSTYPE == "osf1") then
+	# OSF1 == Tru64 on an Alpha:
+		setenv PATH            "${HOME}/bin/osf1"
+		setenv LD_RUN_PATH     "${HOME}/lib/osf1"
+		setenv LD_LIBRARY_PATH "${HOME}/lib/osf1"
+	else if ($OSTYPE == "linux") then
+	# linux == Linux on an Alpha:
+		setenv PATH            "${HOME}/bin/axp"
+		# setenv LD_RUN_PATH     "${HOME}/lib/axp"
+		# setenv LD_LIBRARY_PATH "${HOME}/lib/axp"
+	endif
+#
+# Linux on a intel-PC
+	else if ( $HOSTTYPE == "x86_64" ) then
+		setenv PATH            "${HOME}/bin/x86_64"
+		setenv LD_RUN_PATH     "${HOME}/lib/x86_64"
+		setenv LD_LIBRARY_PATH "${HOME}/lib/x86_64"
+	else if (( $HOSTTYPE == "i386-linux" ) || ( $HOSTTYPE == "i586-linux")) then
+		setenv PATH            "${HOME}/bin/i386:/usr/local/j2sdk/bin"
+		setenv LD_RUN_PATH     "${HOME}/lib/i386"
+		setenv LD_LIBRARY_PATH "${HOME}/lib/i386:/usr/local/intel/compiler80/lib"
+		if (-e /etc/redhat-release) then
+			setenv PATH "${HOME}/bin/i386/rh9:${PATH}"
+		endif
+		if (-e /etc/SuSE-release) then
+			setenv PATH "${HOME}/bin/i386/SuSE:${PATH}"
+		endif
+# HP-UX on a HP-RISC-Workstation
+else if ( $HOSTTYPE == "hp9000s700" ) then
+	setenv PATH            "${HOME}/bin/hpux"
+	setenv LD_RUN_PATH     "${HOME}/lib/hpux"
+	setenv LD_LIBRARY_PATH "${HOME}/lib/hpux"
+	alias make       "gmake"
+
+endif
+#
+# None of the above. In generic there should go only shell scripts
+# else
+# 	setenv PATH "${HOME}/bin/generic"
+# endif
+#
+#----------------------------------------------------------------------
+
+setenv PATH "${PATH}:${HOME}/bin"
+setenv PATH "${PATH}:${HOME}/bin/generic"
+setenv PATH "${PATH}:${HOME}/bin/ROX"
+
+if ( -d /usr/local/alternatives/TeX-Live/bin ) then
+	PATH=$PATH:/usr/local/alternatives/TeX-Live/bin
+fi
+setenv PATH "${PATH}:/usr/local/bin"
+
+if ( -d /opt/wine ) then
+	setenv PATH /opt/wine/bin:${PATH}
+	setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:/opt/wine/lib
+	setenv LD_RUN_PATH     ${LD_RUN_PATH}:/opt/wine/lib
+endif
+
+if ( -d /opt/chess/lib ) then
+	setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:/opt/chess/lib
+	setenv LD_RUN_PATH     ${LD_RUN_PATH}:/opt/chess/lib
+fi
+
+if ( -d /opt/photo/lib ) then
+	setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:/opt/photo/lib
+	setenv LD_RUN_PATH     ${LD_RUN_PATH}:/opt/photo/lib
+fi
+
+setenv PATH "${PATH}:/bin"
+setenv PATH "${PATH}:/usr/bin"
+setenv PATH "${PATH}:/usr/X11R6/bin"
+setenv PATH "${PATH}:/usr/bin/X11"
+setenv PATH "${PATH}:/usr/games"
+setenv PATH "${PATH}:/usr/local/HEP/bin"
+setenv PATH "${PATH}:/usr/local/OHL/bin"
+setenv PATH "${PATH}:/opt/bin"
+setenv PATH "${PATH}:/opt/chess/bin"
+
+# If /etc/PATH exists append it to the current search path
+if ( -e /etc/PATH ) then
+	setenv PATH "${PATH}:`cat /etc/PATH`"
+endif
+#
+# MuPAD needs this, it will not run otherwise!!!
+#
+setenv MuPAD_ROOT_PATH "/usr/local/mupad25"
+setenv PATH "$MuPAD_ROOT_PATH/share/bin:${PATH}"
+
+
---
+++ .path.sh
@@ -0,0 +1,123 @@
+# Set up the path for sh-style shells
+
+append () {
+  var="$1"
+  shift
+  while test $# -gt 0; do
+    if test -d "$1"; then
+      eval "$var"="\$$var:$1"
+      break
+    fi
+    shift
+  done
+}
+
+case "$HOSTTYPE" in
+	*alpha*)
+		case "$OSTYPE" in
+			*osf1*)
+				# OSF1 == Tru64 on an Alpha:
+				PATH="$HOME/bin/osf1"
+				LD_RUN_PATH="$HOME/lib/osf1"
+				LD_LIBRARY_PATH="$HOME/lib/osf1"
+			;;
+			*linux*)
+				# linux == Linux on an Alpha:
+				PATH="$HOME/bin/axp"
+				LD_RUN_PATH="$HOME/lib/axp"
+				LD_LIBRARY_PATH="$HOME/lib/axp"
+			;;
+		esac ;;
+	*x86_64*)
+		# linux on a 64-bit PC
+		PATH="$HOME/bin/x86_64"
+		LD_RUN_PATH="$HOME/lib/x86_64"
+		LD_LIBRARY_PATH="$HOME/lib/x86_64"
+	;;
+	*i[3456]86*)
+		# linux on a x86 PC
+		PATH="$HOME/bin/i386:/usr/local/j2sdk/bin"
+		LD_RUN_PATH="$HOME/lib/i386:/usr/local/intel/compiler/lib"
+		LD_LIBRARY_PATH="$HOME/lib/i386:/usr/local/intel/compiler/lib"
+		# Add Chess software from local scratch if available
+		if [ -e /etc/redhat-release ]; then
+			PATH=$HOME/bin/i386/rh:$PATH
+		fi
+		if [ -e /etc/SuSE-release ]; then
+			PATH=$HOME/bin/i386/SuSE:$PATH
+		fi
+#		if [ -e /scratch/arwagner/chess ]; then
+#			PATH=$PATH:/scratch/arwagner/chess/bin
+#		fi
+		# Instead of Xaw3d or Athena use neXtaw
+		## export LD_PRELOAD=$HOME/lib/i386/libneXtaw.so.0
+	;;
+	*hp*)
+		# HP-UX on a PaRISC-Workstation
+		PATH=$HOME/bin/hpux
+		LD_RUN_PATH=$HOME/lib/hpux
+		LD_LIBRARY_PATH=$HOME/lib/hpux
+	;;
+esac
+
+#
+# None of the above. In generic there should go only shell scripts
+# else
+# 	setenv PATH "${HOME}/bin/generic"
+# fi
+#
+#----------------------------------------------------------------------
+
+PATH=$PATH:$HOME/bin
+PATH=$PATH:$HOME/bin/generic
+PATH=$PATH:$HOME/bin/ROX
+PATH=$PATH:/usr/local/HEP/bin
+
+# Check wether TeX-Live lives in /usr/local/alternatives
+if [ -d /usr/local/alternatives/TeX-Live/bin ]; then
+	PATH=$PATH:/usr/local/alternatives/TeX-Live/bin
+fi
+PATH=$PATH:/usr/local/bin
+
+# Replace systems Wine installation by a self compiled edition
+if [ -h /opt/wine ] || [ -d /opt/wine ]; then
+	PATH=$PATH:/opt/wine/bin
+	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/wine/lib:/opt/wine/lib/wine
+	LD_RUN_PATH=$LD_RUN_PATH:/opt/wine/lib:/opt/wine/lib/wine
+fi
+
+if [ -d /opt/chess/lib ]; then
+	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/chess/lib
+	LD_RUN_PATH=$LD_RUN_PATH:/opt/chess/lib
+fi
+
+if [ -d /opt/photo/lib ]; then
+	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/photo/lib
+	LD_RUN_PATH=$LD_RUN_PATH:/opt/photo/lib
+fi
+
+PATH=$PATH:/bin
+PATH=$PATH:/usr/bin
+PATH=$PATH:/usr/X11R6/bin
+PATH=$PATH:/usr/bin/X11
+PATH=$PATH:/usr/games
+PATH=$PATH:/usr/local/OHL/bin
+PATH=$PATH:/opt/bin
+PATH=$PATH:/opt/chess/bin
+PATH=$PATH:/opt/Go/bin
+
+if [ -f /etc/PATH ]; then
+	PATH=$PATH:`cat /etc/PATH`
+fi 
+
+
+# MuPAD needs this, it will not run otherwise!!!
+#
+MuPAD_ROOT_PATH="/usr/local/mupad25"
+PATH=$MuPAD_ROOT_PATH/share/bin:$PATH
+
+export PATH 
+export LD_RUN_PATH
+export LD_LIBRARY_PATH 
+export MuPAD_ROOT_PATH
+
